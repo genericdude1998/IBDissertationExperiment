587d193899776be72e2c5cb9806f8ce99b96bcda
genericdude1998 <54182558+genericdude1998@users.noreply.github.com>
2021-01-11 12:12:59 +0000

    Add files via upload

---
+++ Role/ObjectRotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;  
+
+public class ObjectRotator : MonoBehaviour
+{
+    private InputDeviceCharacteristics controllerCharacteristics;
+    private InputDevice targetDevice;
+    public float speed;
+    public GameObject currentObjectSpawned;
+   
+
+
+    private void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+        targetDevice = devices[0];
+    }
+
+    private void Update()
+    {
+        if(currentObjectSpawned != null)
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis,out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+        {
+            float rotationAmount = primary2DAxisValue.x * speed * Time.deltaTime;
+
+            currentObjectSpawned.transform.Rotate(transform.rotation.eulerAngles.x, rotationAmount, transform.rotation.eulerAngles.z);
+        }
+    }
+}
---
+++ Role/RoleMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleMenu : MonoBehaviour
+{
+    private RoleTabInteractable[] roleTabsArr; 
+    public RoleTabInteractable m_currentTab;
+    public GameObject currentPrefab;
+    public Transform prefabPos;
+    public Transform prefabPosSmall;
+    public ObjectRotator rotator;
+
+    private Canvas[] canvasArr;
+
+    private void Awake()
+    {
+        roleTabsArr = GetComponentsInChildren<RoleTabInteractable>();
+        canvasArr = GetComponentsInChildren<Canvas>();
+    }
+
+    public void OnClickMenuManager()
+    {
+        CheckMenuCommandsRoleTabInteractor(m_currentTab);
+
+        if (currentPrefab != null)
+            rotator.currentObjectSpawned = currentPrefab;
+        else 
+        {
+            rotator.currentObjectSpawned = null;
+        }
+    }
+
+    void CheckMenuCommandsRoleTabInteractor(RoleTabInteractable roleTab)  // only for the menu controls
+    {
+      
+
+        switch (m_currentTab.name) 
+        {
+            case "BackTab": OpenBackTab(); break;
+            case "ExitTab": OpenExitTab(); break;
+            case "DeleteTab": DeletePrefab(); break;
+        }
+    }
+
+    void OpenBackTab() 
+    {
+        foreach (Canvas canvas in canvasArr) 
+        {
+            if (canvas.name == "MainMenuCanvas" || canvas.name == "TabsCanvas" || canvas.name == "Canvas")
+            { canvas.enabled = true; }
+
+           else { canvas.enabled = false; }
+            
+        }
+    }
+    void OpenExitTab() 
+    {
+        Application.Quit();
+    }
+    void DeletePrefab() 
+    {
+        Destroy(currentPrefab);
+
+    }
+
+}
